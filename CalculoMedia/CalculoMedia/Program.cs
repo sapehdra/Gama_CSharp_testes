bee59d88ec36efdf2c529f6f0f433312166b96e5
sapehdra <36782079+sapehdra@users.noreply.github.com>
2022-08-24 14:50:27 -0300

    Add files via upload
    Cálculo de Média aritmética de notas!

---
+++ CalculoMedia/CalculoMedia/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcumoMedia_23082022
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            int n1, n2, n3, n4;
+
+            //Solicitar dados
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Insira sua Nota 1: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira sua Nota 2: ");
+            n2 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Insira sua Nota 3: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira sua Nota 4: ");
+            n4 = Convert.ToInt32(Console.ReadLine());
+
+            // Mostrar menu de opcoes
+            Console.WriteLine(nome+ " Sua média é: " +(n1+n2+n3+n4)/4);
+
+        }
+    }
+}
