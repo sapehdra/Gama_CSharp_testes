5fc53a21beebd9dedcffac2c65df65678b467e70
sapehdra <36782079+sapehdra@users.noreply.github.com>
2022-08-30 18:20:30 -0300

    Add files via upload
    Treino de Herança em CSharp

---
+++ TipoPessoa/TipoPessoa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoPessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            TipoPessoa TipoPessoa = new TipoPessoa();
+            TipoPessoa.endereco = "Rua da Arena, 300 - CEP: 63400-000";
+            Console.Write("Meu endereço é: " + TipoPessoa.endereco);
+            PessoaFisica.alterEmail("fulano@gmail.com");
+            PessoaFisica.alterEndereco(Console.ReadLine().ToUpper());
+            PessoaFisica.pcd = true;
+
+            PessoaJuridica PessoaJuridica = new PessoaJuridica();
+            PessoaJuridica.alterEmail("sicrano@gmail.com");
+            PessoaJuridica.nome = "ResinAdah";
+            PessoaJuridica.numeroFunc = 15;
+            Console.WriteLine("Voce é pcd: " + PessoaFisica.pcd + " E trabalha na: " + PessoaJuridica.nome);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ TipoPessoa/TipoPessoa/TipoPessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoPessoa
+{
+    public class TipoPessoa
+    {
+        public string nome;
+        public string endereco;
+        public int teleforne;
+        public string email;
+
+        public static void alterEmail(string email)
+        {
+            Console.WriteLine("\nA seu novo email é = " + email);
+        }
+
+        public static void alterEndereco(string endereco)
+        {
+            Console.WriteLine("O seu novo endereço é: " + endereco);
+        }
+    }
+}
